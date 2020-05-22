4ad97d4c756794defee319e56bc2a55168482904
alefo28 <60495655+alefo28@users.noreply.github.com>
2020-05-21 21:19:33 -0500

    practica 11 de estrucutras

---
+++ Practica_11/Practica_11.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define s 30
+struct alumno{
+	int ID;
+	char nom[s];
+	char app[s];
+	char apm[s];
+	char cumple;
+	char escuela[s];
+	int grado;
+	char carrera[s];
+	char gene[s];
+}alum[10];
+
+int main(int argc, char *argv[]) {
+	int i,k,x;
+	
+	for(i=0;i<10;i++){
+		printf("Ingrese el ID del Alumno: ");
+		scanf("%d",&alum[i].ID);
+	
+		if(i==0){
+			for(k=0;k<i;k++){
+				if(alum[i].ID==alum[k].ID){
+					alum[i].ID="XXXX";
+				}
+			}
+		}
+		printf("Ingrese el nombre del Alumno: ");
+		scanf("%s",&alum[i].nom);
+		while(strlen(alum[i].nom)<2){
+			printf("ingrese otro nombre del estudiante: ");
+			scanf("%s",&alum[i].nom);
+		}
+	
+		printf("Ingrese el apellido paterno del Alumno: ");
+		scanf("%s",&alum[i].app);
+		while(strlen(alum[i].app)<2){
+			printf("ingrese otro apellido paterno del estudiante: ");
+			scanf("%s",&alum[i].app);
+		}
+	
+		printf("Ingrese el apellido materno del Alumno: ");
+		scanf("%s",&alum[i].apm);
+		while(strlen(alum[i].apm)<2){
+			printf("ingrese otro apellido materno del estudiante: ");
+			scanf("%s",&alum[i].apm);
+		}
+	
+		printf("Ingrese el cumpleaños del Alumno(DDMMAA): ");
+		scanf("%s",&alum[i].cumple);
+		while(alum[i].cumple < "01011940"){
+			printf("Introduzca una fecha valida: ");
+			scanf("%s",&alum[i].cumple);
+		}
+	
+		printf("Ingrese la escuela del Alumno: ");
+		scanf("%s",alum[i].escuela);
+		while(strlen(alum[i].escuela)<2){
+			printf("ingrese otro nombre de la escuela del estudiante: ");
+			scanf("%s",&alum[i].escuela);
+		}
+	
+		printf("Ingrese el grado del Alumno: ");
+		scanf("%d",alum[i].grado);
+		x=alum[i].grado%1;
+		while(alum[i].grado<1 && x!=0){
+			printf("ingrese un grado valido del estudiante: ");
+			scanf("%d",&alum[i].grado);
+		}
+	
+		printf("Ingrese carrera del Alumno: ");
+		scanf("%s",alum[i].carrera);
+		while(strlen(alum[i].carrera)<2){
+			printf("ingrese otro nombre de carrera valida: ");
+			scanf("%s",&alum[i].carrera);
+		}
+	
+		printf("Ingrese el genero del Alumno: ");
+		scanf("%c",alum[i].gene);
+		while(alum[i].gene!='M' || alum[i].gene!='F'){
+			printf("ingrese un genero valido(M ó F): ");
+			scanf("%c",&alum[i].gene);
+		}
+	}
+	
+	system("cls");
+	for(i=0;i<10;i++){
+		printf("%d	%s	%s	%s	%s	%s	%d	%s	%c",alum[i].ID,alum[i].nom,alum[i].app,alum[i].apm,alum[i].cumple,alum[i].escuela,alum[i].grado,alum[i].carrera,alum[i].gene);
+		printf("\n");
+		
+	}
+	return 0;
+}
